74359a86efab5536d837339e2c6f55dc25e612ef
vroup <simonbalslev@gmail.com>
2018-12-21 01:56:34 +0100

    location strategy to avoid 404 on f5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { FooComponent } from './foo/foo.component';
-import { BarComponent } from './bar/bar.component';
-import { QuestionsComponent } from './questions/questions.component';
-import { QuestionComponent } from './question/question.component';
-import { NewQuestionComponent } from './new-question/new-question.component';
-import { LoginComponent } from './login/login.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { RestaurantComponent } from './restaurant/restaurant.component';
-import { RegisterComponent } from './register/register.component';
-import { RatingComponent } from './rating/rating.component';
-import { StarComponent } from './star/star.component';
-import { StarHostDirective } from './star-host.directive';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {FooComponent} from './foo/foo.component';
+import {BarComponent} from './bar/bar.component';
+import {QuestionsComponent} from './questions/questions.component';
+import {QuestionComponent} from './question/question.component';
+import {NewQuestionComponent} from './new-question/new-question.component';
+import {LoginComponent} from './login/login.component';
+import {LandingPageComponent} from './landing-page/landing-page.component';
+import {RestaurantComponent} from './restaurant/restaurant.component';
+import {RegisterComponent} from './register/register.component';
+import {RatingComponent} from './rating/rating.component';
+import {StarComponent} from './star/star.component';
+import {StarHostDirective} from './star-host.directive';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,8 @@ import { StarHostDirective } from './star-host.directive';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
